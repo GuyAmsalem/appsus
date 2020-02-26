81a56584e51898788a20e60ed175834fdd19bf8c
Guy Amsalem <you@example.com>
2020-02-26 12:47:28 +0200

    first commit

---
+++ js/services/event-bus.service.js
@@ -0,0 +1,6 @@
+
+export const EVENT_SHOW_MSG = 'showMsg';
+export const eventBus = new Vue()
+
+
+
---
+++ js/services/util.service.js
@@ -0,0 +1,66 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function makeId(length = 5) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function getTodayDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
+
+function makeLorem(length) {
+
+    var randStr = '';
+    while (randStr.length < length) {
+        var wordLength = getRandomInt(3, 6);
+        var word = _createWord(wordLength);
+
+        if (Math.random() > 0.9) word += ',';
+
+        randStr += word + ' ';
+    }
+    randStr = randStr.substring(0, length);
+    randStr = randStr[0].toUpperCase() + randStr.substr(1)
+
+    return randStr;
+}
+
+export const utilService = {
+    getRandomInt,
+    makeLorem,
+    makeId,
+    getTodayDate
+}
+
+
+function _getRandChar() {
+    var LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+    var randIndex = parseInt(Math.random() * LETTERS.length)
+    return LETTERS.charAt(randIndex);
+}
+
+function _createWord(length) {
+    var word = '';
+    while (word.length < length) {
+        var randChar = _getRandChar();
+        word += randChar;
+    }
+
+    return word;
+}
