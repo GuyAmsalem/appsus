6dc54ec0a042211412b9a56c82d45665fe52114d
RZalmon <ranzlmn@gmail.com>
2020-03-03 09:22:10 +0200

    fixed about spelling mistake

--- js/general/pages/about.cmp.js
+++ js/general/pages/about.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
         <section class="about-section">
             <h2>Hi all!</h2>
-            <p>Greetings from Ran and Guy. We are proud to represnt you Appsus!
+            <p>Greetings from Ran and Guy. We are proud to present you Appsus!
                 Appsus was created as part of Coding Academy third Sprint, focusing
                 on Vue.js Framework and Responsive Design.
                 Appsus contains a demo Email app and a Notes Keeper app. We are hoping that you will
